d5147e0281d7f21209eca21db39da365bec934c4
sajjad <sajjad.goodarzi1990@gmail.com>
2023-03-02 21:47:35 +0000

    done

--- src/main/java/IsSorted.java
+++ src/main/java/IsSorted.java
@@ -7,6 +7,23 @@ public class IsSorted {
      * @return true if arr is sorted (all the numbers are in order). false if arr is not sorted.
      */
     public boolean checkSort(int[] arr){
-        return false;
+        
+            for (int i = 0; i < arr.length - 1; i++) {
+               if (arr[i] < arr[i + 1]) {
+                return true;
+              
+            }else
+            return false;
+        
+        }
+            return false;
     }
-}
+}       
+       
+        
+        
+        
+        
+        
+    
+
